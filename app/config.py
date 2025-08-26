5aa9638708b19ff878a20da918c9a4c684c6a841
GitHub Action <action@github.com>
2025-08-26 12:27:33 +0000

    Auto-update versions to 1.0.52 [skip ci]

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ import os
 class Settings(BaseSettings):
     # Application settings
     app_name: str = "Airia Infrastructure Test Pod"
-    version: str = "1.0.50"
+    version: str = "1.0.52"
     
     # Authentication settings
     auth_username: str = os.getenv("AUTH_USERNAME", "admin")
